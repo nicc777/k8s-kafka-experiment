23091c6e9f1f232034f390fb7f2a059f73020fd5
Nico Coetzee <nicc777@gmail.com>
2024-10-16 05:17:49 +0200

    Adapting producer to use pre-existing schema version from the schema registry

--- backend-service/v1/raw_data_generator/main.py
+++ backend-service/v1/raw_data_generator/main.py
@@ -6,8 +6,8 @@ import time
 import json
 import sys
 import traceback
-from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
+from confluent_kafka.avro.cached_schema_registry_client import CachedSchemaRegistryClient
 
 
 HOSTNAME = socket.gethostname()
@@ -20,6 +20,15 @@ MAX_INTERVAL_PER_LOOP = 999999999 / MAX_RATE_PER_SECOND
 SLEEP_BUFFER = 10
 REPORT_RATE_INTERVAL_TICKS = int(os.getenv('REPORT_RATE_INTERVAL_TICKS', '60'))
 KAFKA_HOST = ''
+KAFKA_BOOTSTRAP_SERVER_HOST = os.getenv('KAFKA_BOOTSTRAP_SERVER', 'localhost')
+KAFKA_BOOTSTRAP_SERVER_PORT = os.getenv('KAFKA_BOOTSTRAP_SERVER', '9092')
+KAFKA_SCHEMA_SERVER_HOST = os.getenv('KAFKA_BOOTSTRAP_SERVER', 'localhost')
+KAFKA_SCHEMA_SERVER_PORT = os.getenv('KAFKA_BOOTSTRAP_SERVER', '8081')
+KAFKA_SCHEMA_SERVER_PROTOCOL = os.getenv('KAFKA_BOOTSTRAP_SERVER', 'http')
+KAFKA_TOPIC = os.getenv('KAFKA_TOPIC', 'raw-data-in')
+SCHEMA_VERSION = int(os.getenv('SCHEMA_VERSION', '1'))
+SCHEMA_SUBJECT = os.getenv('SCHEMA_SUBJECT', 'TestRawData')
+SCHEMA_NAMESPACE = os.getenv('SCHEMA_SUBJECT', 'tld.example')
 
 
 logger = logging.getLogger('raw_data_generator')
@@ -34,22 +43,40 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 logger.info('{} - SKU: {}'.format(HOSTNAME, SKU))
-logger.debug('{} - MAX_RATE_PER_SECOND: {}'.format(HOSTNAME, MAX_RATE_PER_SECOND))
-logger.debug('{} - MAX_INTERVAL_PER_LOOP: {}'.format(HOSTNAME, MAX_INTERVAL_PER_LOOP))
-logger.debug('{} - SLEEP_BUFFER: {}'.format(HOSTNAME, SLEEP_BUFFER))
+logger.debug('{} - MAX_RATE_PER_SECOND          : {}'.format(HOSTNAME, MAX_RATE_PER_SECOND))
+logger.debug('{} - MAX_INTERVAL_PER_LOOP        : {}'.format(HOSTNAME, MAX_INTERVAL_PER_LOOP))
+logger.debug('{} - SLEEP_BUFFER                 : {}'.format(HOSTNAME, SLEEP_BUFFER))
+logger.debug('{} - KAFKA_BOOTSTRAP_SERVER_HOST  : {}'.format(KAFKA_BOOTSTRAP_SERVER_HOST))
+logger.debug('{} - KAFKA_BOOTSTRAP_SERVER_PORT  : {}'.format(KAFKA_BOOTSTRAP_SERVER_PORT))
+logger.debug('{} - KAFKA_SCHEMA_SERVER_PROTOCOL : {}'.format(KAFKA_SCHEMA_SERVER_PROTOCOL))
+logger.debug('{} - KAFKA_SCHEMA_SERVER_HOST     : {}'.format(KAFKA_SCHEMA_SERVER_HOST))
+logger.debug('{} - KAFKA_SCHEMA_SERVER_PORT     : {}'.format(KAFKA_SCHEMA_SERVER_PORT))
 
 
-config = {
-    'bootstrap.servers': 'localhost:9092',
-    'schema.registry.url': 'http://localhost:8081'
+KAFKA_SERVER_CONNECTION_CONFIG = {
+    'bootstrap.servers': '{}:{}'.format(KAFKA_BOOTSTRAP_SERVER_HOST, KAFKA_BOOTSTRAP_SERVER_PORT),
+    'schema.registry.url': '{}://{}:{}'.format(KAFKA_SCHEMA_SERVER_PROTOCOL, KAFKA_SCHEMA_SERVER_HOST, KAFKA_SCHEMA_SERVER_PORT)
 }
-try:
-    value_schema = avro.loads('{"type": "record", "name": "Value", "fields": [{"name": "name", "type": "string"}]}')
-    producer = AvroProducer(config, default_value_schema=value_schema)
-except:
-    logger.error('EXCEPTION: {}'.format(traceback.format_exc()))
-    time.sleep(120)
-    sys.exit(1)
+
+# Initialize Schema Registry client
+schema_registry_client = CachedSchemaRegistryClient(KAFKA_SERVER_CONNECTION_CONFIG['schema.registry.url'])
+
+
+# Function to load specific schema version
+def load_schema(subject_name, version):
+    return schema_registry_client.get_version(subject_name, version).schema
+                                              
+
+def produce_with_schema(topic, data, schema_subject, schema_version):
+    schema = load_schema(schema_subject, schema_version)
+    avro_producer = AvroProducer(KAFKA_SERVER_CONNECTION_CONFIG, default_value_schema=schema)
+
+    if schema.validate(data):
+        avro_producer.produce(topic=topic, value=data)
+        avro_producer.flush()
+        print(f"Message with schema version {schema_version} sent successfully!")
+    else:
+        print(f"Data validation failed for schema version {schema_version}.")
 
 
 now_ns = time.time_ns()
@@ -67,8 +94,15 @@ while True:
     }
     logger.info('{} - DATA: {}'.format(HOSTNAME, json.dumps(raw_data, default=str)))
     try:
-        producer.produce(topic='test_topic', value={"name": "Alice"})
-        producer.flush()
+        produce_with_schema(
+            KAFKA_TOPIC,
+            raw_data,
+            '{}.{}-value'.format(
+                SCHEMA_NAMESPACE,
+                SCHEMA_SUBJECT
+            ),
+            SCHEMA_VERSION
+        )
     except:
         logger.error('EXCEPTION: {}'.format(traceback.format_exc()))
     now_ns_step = time.time_ns()
