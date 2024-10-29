71873a9a34a553af72345b16b2a9de5423902a32
Nico Coetzee <nicc777@gmail.com>
2024-10-29 04:35:05 +0100

    Added fixme for deployment of configmap

--- backend-service/build.sh
+++ backend-service/build.sh
@@ -3,7 +3,7 @@
 # Checkout our projectcd
 cd /tmp/k8s-kafka-experiment/backend-service
 
-ehoc "- Current directory: ${PWD}"
+echo "- Current directory: ${PWD}"
 
 # Set version with:
 #   export APP_VERSION=vN 
@@ -14,6 +14,7 @@ echo "- Building application version ${APP_VERSION}"
 
 echo "- Packaging Configmap's for Python files"
 
+# FIXME - I need another approach, as I cannot commit and push these changes....
 echo "  - raw_data_generator"
 kubectl create configmap raw-data-generator-cm --from-file=$APP_VERSION/raw_data_generator/main.py --dry-run=client -o yaml > /tmp/k8s-kafka-experiment/experiments/argocd/deployments/cell-app/$APP_VERSION/deployment.yaml
 kubectl describe configmap raw_data_generator-main-cm -n exp
