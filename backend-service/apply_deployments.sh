bcf7e68881bcf461f20142a81e35b66306591546
Nico Coetzee <nicc777@gmail.com>
2024-10-30 09:29:50 +0100

    Added schema v1 to schema registry and updated deployment scripts

--- backend-service/apply_deployments.sh
+++ backend-service/apply_deployments.sh
@@ -5,6 +5,8 @@ kubectl apply -f backend-service/argocd/application/application_version_$APP_VER
 FILE="backend-service/argocd/application/application_version_$APP_VERSION.yaml"
 if [ -e "$FILE" ]; then
     echo "  - $FILE exists."
+    kubectl apply -f backend-service/argocd/application/kafka_schemas_$APP_VERSION.yaml -n argocd || true
+    sleep 10
     kubectl apply -f backend-service/argocd/application/application_version_$APP_VERSION.yaml -n argocd || true
 else
     echo "  - $FILE does not exist."
